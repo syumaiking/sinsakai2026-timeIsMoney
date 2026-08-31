367cb0774799a442394f1a61b946b20473652894
syumaiking <noguchisyudai0310@gmail.com>
2026-08-31 09:48:01 +0900

    Menu,Main,Shift,ShiftService,InputUtilクラスの追加、およびShiftクラスの機能追加

---
+++ Time is money/src/model/Shift.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Shift {
+	private int id;
+	private LocalDate workDate;
+	private LocalTime startTime;
+	private LocalTime endTime;
+	private int breakTime;
+	private int hourlyWage;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getWorkDate() {
+		return workDate;
+	}
+
+	public void setWorkDate(LocalDate workDate) {
+		this.workDate = workDate;
+	}
+
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public int getBreakTime() {
+		return breakTime;
+	}
+
+	public void setBreakTime(int breakTime) {
+		this.breakTime = breakTime;
+	}
+
+	public int getHourlyWage() {
+		return hourlyWage;
+	}
+
+	public void setHourlyWage(int hourlyWage) {
+		this.hourlyWage = hourlyWage;
+	}
+
+}
