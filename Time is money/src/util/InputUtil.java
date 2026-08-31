9e387f1b1a76f69d6091c60147831abef975eb43
syumaiking <noguchisyudai0310@gmail.com>
2026-08-31 10:03:54 +0900

    InputUtilクラスの機能追加

--- Time is money/src/util/InputUtil.java
+++ Time is money/src/util/InputUtil.java
@@ -1,5 +1,27 @@
 package util;
 
-public class InputUtil {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
 
+public class InputUtil {
+	public int inputInt(String message) {
+		System.out.println(message);
+		int number = Scanner.nextInt();
+		return number;
+	}
+
+	public LocalDate inputDate(String message) {
+		System.out.println(message);
+		String input = Scanner.nextLine();
+		LocalDate date = LocalDate.parse(input);
+		return date;
+	}
+
+	public LocalTime inputTime(String message) {
+		System.out.println(message);
+		String input = Scanner.nextLine();
+		LocalTime time = LocalTime.parse(input);
+		return time;
+	}
 }
